e0e8883aa9ea91f668094c6a381b0fd354d77b30
Andrei Bukhavets <x10c@duck.com>
2025-04-21 12:04:38 +0300

    new cards

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -8,10 +8,18 @@ export const IndexPage: FC = () => {
             <input type="search" placeholder="Искать в Shparka" />
         </div>
         <div className="container">
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square"></div>
-          <div className="square gradient"></div>
+          <div className="square ready">
+            <div className="square-text">Готовая еда</div>
+          </div>
+          <div className="square water">
+            <div className="square-text">Вода и напитки</div>
+          </div>
+          <div className="square snacks">
+            <div className="square-text">Снеки</div>
+          </div>
+          <div className="square gradient">
+            <div className="square-text text-white">Готовая еда</div>
+          </div>
         </div>
         <div className="new-heading">Новинки</div>
         <div className="meal-container">
