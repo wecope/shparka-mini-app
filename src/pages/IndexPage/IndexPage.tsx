1c4f14715676bcc6028a2d88dc0c23524f9e4c78
Andrei Bukhavets <x10c@duck.com>
2025-05-27 12:33:51 +0300

    new cards

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -5,6 +5,7 @@ export const IndexPage: FC = () => {
   return (
     <Page back={false}>
         <div className="search-bar">
+            <div className="search-icon"></div>
             <input type="search" placeholder="Искать в Shparka" />
         </div>
         <div className="container">
@@ -23,12 +24,36 @@ export const IndexPage: FC = () => {
         </div>
         <div className="new-heading">Новинки</div>
         <div className="meal-container">
-            <div className="meal-square"></div>
-            <div className="meal-square"></div>
-            <div className="meal-square"></div>
-            <div className="meal-square"></div>
-            <div className="meal-square"></div>
-            <div className="meal-square"></div>
+            <div className="meal-card">
+                <div className="meal-square-1"></div>
+                <div className="meal-text-1">Омлет с помидорами</div>
+                <div className="meal-weight-1">400 г</div>
+                <div className="meal-price-1">
+                    <div className="meal-price-text-1">6,90</div>
+                    <div className="meal-price-plus-1">+</div>
+                </div>
+            </div>
+            <div className="meal-card">
+                <div className="meal-square-2"></div>
+                <div className="meal-text-2">Омлет с помидорами</div>
+                <div className="meal-weight-2">400 г</div>
+                <div className="meal-price-2">
+                    <div className="meal-price-text-2">6,90</div>
+                    <div className="meal-price-plus-2">+</div>
+                </div>
+            </div>
+            <div className="meal-card">
+                <div className="meal-square-3"></div>
+                <div className="meal-text-3">Омлет с помидорами</div>
+                <div className="meal-weight-3">400 г</div>
+                <div className="meal-price-3">
+                    <div className="meal-price-text-3">6,90</div>
+                    <div className="meal-price-plus-3">+</div>
+                </div>
+            </div>
+            <div className="meal-square-4"></div>
+            <div className="meal-square-5"></div>
+            <div className="meal-square-6"></div>
         </div>
     </Page>
   );
